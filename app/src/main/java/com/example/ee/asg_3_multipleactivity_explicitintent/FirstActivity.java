91f7bcfd4dd5748c5d67ec1015ba5ece6fe43785
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-08-04 12:50:01 +0500

    message transfer from first activity to second activity

--- app/src/main/java/com/example/ee/asg_3_multipleactivity_explicitintent/FirstActivity.java
+++ app/src/main/java/com/example/ee/asg_3_multipleactivity_explicitintent/FirstActivity.java
@@ -1,9 +1,16 @@
 package com.example.ee.asg_3_multipleactivity_explicitintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    Button sendButton;
+    EditText sendText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,26 @@ public class FirstActivity extends AppCompatActivity {
 
         setTitle(R.string.first_activity_name);
 
+        sendButton = (Button) findViewById(R.id.sendButton);
+        sendText = (EditText) findViewById(R.id.L1_editText);
+
+    }
+
+    public  void sendMessage(View v){
+
+        Intent intent = new Intent(this,SecondActivity.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString("MESSAGE",String.valueOf(sendText.getText()));
+
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sendText.getText().clear();
     }
 }
--- app/src/main/java/com/example/ee/asg_3_multipleactivity_explicitintent/SecondActivity.java
+++ app/src/main/java/com/example/ee/asg_3_multipleactivity_explicitintent/SecondActivity.java
@@ -1,9 +1,13 @@
 package com.example.ee.asg_3_multipleactivity_explicitintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView disp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +15,14 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         setTitle(R.string.second_activity_name);
+
+        disp = (TextView) findViewById(R.id.L2_messageDisp);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        String msg = bundle.getString("MESSAGE");
+
+        disp.setText(msg);
     }
 }
